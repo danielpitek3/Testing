379c64bf40181deb415039e30725e5475aa3e39d
Daniel Pitek <Daniel813245@outlook.sk>
2021-12-07 11:45:27 +0100

    alpha test//cc v0.2 fix

--- ForgottenHeroGame/Form1.Designer.cs
+++ ForgottenHeroGame/Form1.Designer.cs
@@ -29,12 +29,132 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.fhero = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameEnd = new System.Windows.Forms.Label();
+            this.gameOver = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fhero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.Location = new System.Drawing.Point(-14, 405);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(837, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ForgottenHeroGame.Properties.Resources.obstacle_2;
+            this.pictureBox4.Location = new System.Drawing.Point(698, 373);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ForgottenHeroGame.Properties.Resources.obstacle_1;
+            this.pictureBox3.Location = new System.Drawing.Point(398, 360);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // fhero
+            // 
+            this.fhero.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.fhero.Image = global::ForgottenHeroGame.Properties.Resources.running;
+            this.fhero.Location = new System.Drawing.Point(48, 367);
+            this.fhero.Name = "fhero";
+            this.fhero.Size = new System.Drawing.Size(40, 43);
+            this.fhero.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.fhero.TabIndex = 1;
+            this.fhero.TabStop = false;
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("AcmeFont", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(285, 25);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(223, 60);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameEnd
+            // 
+            this.gameEnd.AutoSize = true;
+            this.gameEnd.Font = new System.Drawing.Font("November", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameEnd.Location = new System.Drawing.Point(222, 109);
+            this.gameEnd.Name = "gameEnd";
+            this.gameEnd.Size = new System.Drawing.Size(0, 60);
+            this.gameEnd.TabIndex = 5;
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.Font = new System.Drawing.Font("Notram", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameOver.Location = new System.Drawing.Point(256, 224);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(0, 28);
+            this.gameOver.TabIndex = 6;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gameOver);
+            this.Controls.Add(this.gameEnd);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.fhero);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fhero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox fhero;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Label gameEnd;
+        private System.Windows.Forms.Label gameOver;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- ForgottenHeroGame/Form1.cs
+++ ForgottenHeroGame/Form1.cs
@@ -12,9 +12,171 @@ namespace ForgottenHeroGame
 {
     public partial class Form1 : Form
     {
+        // variables
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            fhero.Top += jumpSpeed;
+
+            scoreText.Text = "Score: " + score;
+
+            if (jumping == true && force < 0) // jump
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+
+            if (fhero.Top > 366 && jumping == false)
+            {
+                force = 12;
+                fhero.Top = 367;
+                jumpSpeed = 0;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+
+                    if (fhero.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        fhero.Image = Properties.Resources.dead;
+
+                        // GAME END SIGNS //
+                        if (score < 5)
+                        {
+                            gameEnd.Text = "- GAME OVER -";
+                            scoreText.Text = "Score : " + score;
+                            gameOver.Text = "Level: Noob - Press R";
+                            isGameOver = true;
+                        }
+                        else if (score < 10)
+                        {
+                            gameEnd.Text = "- GAME OVER -";
+                            scoreText.Text = "Score : " + score;
+                            gameOver.Text = "Level: Normal - Press R";
+                            isGameOver = true;
+                        }
+                        else if (score < 20)
+                        {
+                            gameEnd.Text = "- GAME OVER -";
+                            scoreText.Text = "Score : " + score;
+                            gameOver.Text = "Level: Good - Press R";
+                            isGameOver = true;
+                        }
+                        else if (score < 30)
+                        {
+                            gameEnd.Text = "- GAME OVER -";
+                            scoreText.Text = "Score : " + score;
+                            gameOver.Text = "Level: Pro - Press R";
+                            isGameOver = true;
+                        }
+                        else if (score > 30)
+                        {
+                            gameEnd.Text = "- GAME OVER -";
+                            scoreText.Text = "Score : " + score;
+                            gameOver.Text = "Level: Insane - Press R";
+                            isGameOver = true;
+                        }
+                    }
+                }
+            }
+
+            if (score > 5) // SPEED
+            {
+                obstacleSpeed = 15;
+            }
+            else if (score > 10)
+            {
+                obstacleSpeed = 20;
+            }
+            else if (score > 20)
+            {
+                obstacleSpeed = 25;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false) // method for jumping down
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (jumping == true) // method for jumping up
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+        private void GameReset()
+        {
+            // Game reset //
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            scoreText.Text = "Score: " + score;
+            gameEnd.Text = " ";
+            gameOver.Text = " ";
+            fhero.Image = Properties.Resources.running; // texture change from dead to running //
+            isGameOver = false;
+            fhero.Top = 367;
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start(); // start of the game //
+
+        }
+
     }
 }
+
